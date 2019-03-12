0947d91168dc845257966246514c3a2857e2d31c
jess compacion <jagcompacion@gmail.com>
2019-03-12 23:57:05 +0700

    Updates

---
+++ precache-manifest.be80a98eade043790cb7a2fb34012f14.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fdb9fa4b4e27890ebda95d052096f31",
+    "url": "/portfolio/static/media/skype-logo.6fdb9fa4.svg"
+  },
+  {
+    "revision": "7658ffb25432b6205a9594bf65a8ff16",
+    "url": "/portfolio/static/media/linkedin-logo.7658ffb2.svg"
+  },
+  {
+    "revision": "d3bc903cc9299df003ce3103bad39c34",
+    "url": "/portfolio/static/media/gmail-logo.d3bc903c.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/portfolio/static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.784d01ab.js"
+  },
+  {
+    "revision": "8d3e672ef3b6be52a56f",
+    "url": "/portfolio/static/js/main.f2b218a5.chunk.js"
+  },
+  {
+    "revision": "487f6e9352605fb6f3ad",
+    "url": "/portfolio/static/js/2.833e2598.chunk.js"
+  },
+  {
+    "revision": "8d3e672ef3b6be52a56f",
+    "url": "/portfolio/static/css/main.15d57c20.chunk.css"
+  },
+  {
+    "revision": "487f6e9352605fb6f3ad",
+    "url": "/portfolio/static/css/2.ae864961.chunk.css"
+  },
+  {
+    "revision": "d5033871c955826f98888d158924c2dd",
+    "url": "/portfolio/index.html"
+  }
+];
